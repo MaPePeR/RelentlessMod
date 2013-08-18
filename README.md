17f76027b525b7864ad90c297f10a70f41b6b7d1
RelentlessEmu <neiko.ambrose@gmail.com>
2013-08-19 04:33:00 +1200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+## Relentless Summoner Mod
+This Summoner mod is required to connect to a Relentless server.
---
+++ RelentlessMod/Relentless.cs
@@ -0,0 +1,57 @@
+﻿using Mono.Cecil;
+using ScrollsModLoader.Interfaces;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace Relentless
+{
+    public class Relentless : BaseMod
+    {
+        string serverHost = "";
+        string serverKey  = "";
+
+        public static string GetName()
+        {
+            return "Relentless";
+        }
+
+        public static int GetVersion()
+        {
+            return 1;
+        }
+
+        public static MethodDefinition[] GetHooks(TypeDefinitionCollection scrollsTypes, int version)
+        {
+            try
+            {
+                return new MethodDefinition[]
+                {
+                    scrollsTypes["Communicator"].Methods.GetMethod("Awake")[0]
+                };
+            }
+            catch
+            {
+                return new MethodDefinition[] { };
+            }
+        }
+
+        public override void BeforeInvoke(InvocationInfo info)
+        {
+            if (info.targetMethod.Equals("Awake"))
+            {
+                typeof(Communicator).GetField("publicKeyXml", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(info.target, serverKey);
+            }
+        }
+
+        public override void AfterInvoke(InvocationInfo info, ref object returnValue)
+        {
+            if (info.targetMethod.Equals("Awake"))
+            {
+                typeof(Communicator).GetField("lobbyIpAddress", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(info.target, serverHost);
+            }
+        }
+    }
+}
+
